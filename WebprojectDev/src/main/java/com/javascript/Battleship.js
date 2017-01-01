473f52a9dca7ed898c32cb9ce495f28c98fa04bc
surverules <surverules@gmail.com>
2017-01-01 12:23:15 -0600

    Including Images and code for the Battleship.js

--- WebprojectDev/src/main/java/com/javascript/Battleship.js
+++ WebprojectDev/src/main/java/com/javascript/Battleship.js
@@ -16,13 +16,6 @@ var view = {
     }
 }
 
-
-var ships = [
-        {location: [01, 11, 21], hits: ["hit", "", ""]},
-        {location: [03, 13, 23], hits: ["", "", ""]},
-        {location: [04, 14, 24], hits: ["", "", ""]}
-        ]
-
 function  checkOption(location) {
     var ArrayChar = ["A", "B", "C", "D", "E", "F", "G"];
     for (var i = 0; i < ArrayChar.length; i ++) {
@@ -31,3 +24,43 @@ function  checkOption(location) {
         }
     }
 }
+
+var model = {
+    boardSize: 7,
+    numShips: 3,
+    shipLenght: 3,
+    shipsSunk: 0,
+    ships : [
+            {location: [01, 11, 21], hits: ["", "", ""]},
+            {location: [03, 13, 23], hits: ["", "", ""]},
+            {location: [04, 14, 24], hits: ["", "", ""]}
+        ],
+    fire: function (incodedLocation){
+            var i = 0;
+            while(i < this.ships.length) {
+                var ship = this.ships[i];
+                var index = ship.location.indexOf(incodedLocation)
+                if (index >=0) {
+                    ship.hits[index] = "hit";
+                    view.setPropertyForHit(incodedLocation);
+                    if (this.isSunk(ship)){
+                        this.shipsSunk ++;
+                    }
+                    return true;
+                }
+            }
+            view.setPropertyForMiss(incodedLocation);
+            return false;
+            },
+    isSunk: function (ship){
+        for (i = 0; i <=ship.hits.length; i++){
+            if (ship.hits[i] != "hit"){
+                return false;
+            }
+        }
+        return true;
+    }
+}
+
+model.fire(01);
+model.fire(02);
